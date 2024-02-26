ada180f9d27a3d41d904d47389abe99bb6218903
Mihir Karandikar <mihirkkarandikar@gmail.com>
2024-02-26 18:52:24 +0530

    asdasd

--- graph-based-modelling-benchmarking/benchmark.py
+++ graph-based-modelling-benchmarking/benchmark.py
@@ -37,7 +37,7 @@ if __name__ == "__main__":
 
         with open("output.txt", "w+") as f:
             for num_test_samples in [1, 5, 10, 20, 50, 100]:
-                for _ in range(5):
+                for _ in range(3):
                     lb = random.randint(0, total_samples - num_test_samples - 1)
                     assert (
                         lb >= 0
@@ -53,16 +53,19 @@ if __name__ == "__main__":
 
                     avg_len = get_avg_len(data_curr)
 
-                    num_iters = 3
-                    execution_time = timeit.timeit(
-                        lambda: gb.get_predictions(data_curr),
-                        number=num_iters,
-                    )
-                    execution_time /= num_iters
-                    print(
-                        f"Execution time for {num_test_samples} samples and avg len {avg_len}: {execution_time} seconds"
-                    )
-                    f.write(f"{num_test_samples} {avg_len} {execution_time}\n")
-
+                    try:
+                        num_iters = 1
+                        execution_time = timeit.timeit(
+                            lambda: gb.get_predictions(data_curr),
+                            number=num_iters,
+                        )
+                        execution_time /= num_iters
+                        print(
+                            f"Execution time for {num_test_samples} samples and avg len {avg_len}: {execution_time} seconds"
+                        )
+                        f.write(f"{num_test_samples} {avg_len} {execution_time}\n")
+                    except:
+                        print("ERROR")
+                        continue
         
 
